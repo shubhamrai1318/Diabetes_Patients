b061955dfa572eb3ded2f147c3b92401ca84af18
Shubham Rai <shubhamrai1318@gmail.com>
2023-12-02 22:32:33 +0530

    Update DiabetesCheckup.py

--- DiabetesCheckup.py
+++ DiabetesCheckup.py
@@ -73,8 +73,8 @@ def user_report():
 
 # PATIENT DATA
 user_data = user_report()
-#st.subheader(':orange[Calculating Report for this Patient]')
-st.subheader("<span style='color: orange;'>Calculating Report for this Patient</span>", unsafe_allow_html=True)
+st.subheader(':orange[Calculating Report for this Patient]')
+#st.subheader("<span style='color: orange;'>Calculating Report for this Patient</span>", unsafe_allow_html=True)
 
 st.write(user_data)
 
